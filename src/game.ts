7b95c243a35e458637d5626f756263ba56be4071
rch850 <rich850@gmail.com>
2022-09-19 22:38:37 +0900

    Tweak game difficulty

--- src/game.ts
+++ src/game.ts
@@ -5,17 +5,17 @@
  */
 
 /** 姿勢推定のしきい値。高いほど厳しくなる */
-const SCORE_THRESHOLD = 0.5;
+const SCORE_THRESHOLD = 0.8;
 
 /** 出題から判定までの待ち時間 */
-const JUDGE_WAIT_SECONDS = 2;
+const JUDGE_WAIT_SECONDS = 1.5;
 
 /** 姿勢をこの秒数保ったらその姿勢になったとする */
-const POSE_KEEP_SECONDS = 0.5;
+const POSE_KEEP_SECONDS = 0.3;
 
 let prevPose = [false, false];
 let poseStartTime = 0;
-let questionStartTime = 0;
+let judgeStartTime = 0;
 let answerPose = [false, false];
 let soundCorrect = document.querySelector<HTMLAudioElement>('#sound-correct')!;
 
@@ -43,12 +43,11 @@ export function onPoseDetected(poses) {
     poseStartTime = Date.now();
   }
 
-  if (questionStartTime === 0) {
-    questionStartTime = Date.now();
+  if (judgeStartTime === 0) {
     makeQuestion();
   }
 
-  if (questionStartTime + JUDGE_WAIT_SECONDS * 1000 < Date.now()) {
+  if (judgeStartTime < Date.now()) {
     // 出題から一定時間が経過したので、判定開始
     if (poseStartTime + POSE_KEEP_SECONDS * 1000 < Date.now()) {
       if (answerPose[0] === prevPose[0] && answerPose[1] === prevPose[1]) {
@@ -57,7 +56,6 @@ export function onPoseDetected(poses) {
         soundCorrect.currentTime = 0;
         soundCorrect.play();
         makeQuestion();
-        questionStartTime = Date.now();
       }
     }
   }
@@ -72,16 +70,29 @@ function makeQuestion() {
     if (randomFlag < 0.7) {
       answerPose[0] = !answerPose[0];
       gameState.question = answerPose[0] ? 'しろあげて' : 'しろさげて';
+      judgeStartTime = Date.now();
     } else {
+      if (Math.random() < 0.5) {
+        gameState.question = answerPose[0] ? 'しろさげない' : 'しろあげない';
+      } else {
+        gameState.question = answerPose[0] ? 'しろさげないで' : 'しろあげないで';
+      }
       gameState.question = answerPose[0] ? 'しろさげないで' : 'しろあげないで';
+      judgeStartTime = Date.now() + JUDGE_WAIT_SECONDS * 1000;
     }
   } else {
     // あか
     if (randomFlag < 0.7) {
       answerPose[1] = !answerPose[1];
       gameState.question = answerPose[1] ? 'あかあげて' : 'あかさげて';
+      judgeStartTime = Date.now();
     } else {
-      gameState.question = answerPose[1] ? 'あかさげないで' : 'あかあげないで';
+      if (Math.random() < 0.5) {
+        gameState.question = answerPose[1] ? 'あかさげない' : 'あかあげない';
+      } else {
+        gameState.question = answerPose[1] ? 'あかさげないで' : 'あかあげないで';
+      }
+      judgeStartTime = Date.now() + JUDGE_WAIT_SECONDS * 1000;
     }
   }
   // あかしろあげて、などは未実装
